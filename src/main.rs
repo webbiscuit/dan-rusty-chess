89070554c0e4ba64bfb76753befafa746b317463
Daniel <daniel@webbiscuit.co.uk>
2021-10-31 20:46:35 +0000

    Show correct glyphs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::str;
 
 struct ChessBoard {
-    board: [u32; 64],
+    board: [Option<char>; 64],
 }
 
 impl ChessBoard {
     pub fn new() -> ChessBoard {
-        ChessBoard { board: [0; 64] }
+        ChessBoard { board: [None; 64] }
     }
     fn to_board_index(file: u32, rank: u32) -> u32 {
         return rank * 8 + file;
     }
     fn from_fen(fen: &str) -> ChessBoard {
-        let mut board = ChessBoard { board: [0; 64] };
+        let mut board = ChessBoard::new();
 
         let mut fen_sections = fen.split(" ");
 
@@ -29,57 +29,62 @@ impl ChessBoard {
                 file = 0;
             } else {
                 let ix = ChessBoard::to_board_index(file, rank);
-                board.board[ix as usize] = 1; //piece_placement;
+                board.board[ix as usize] = Some(piece_placement); //piece_placement;
                 file += 1;
             }
         }
 
         return board;
-
-        // self.board[1] = 1;
     }
-    fn get_rank(&self, rank: usize) -> &[u32] {
+    fn get_rank(&self, rank: usize) -> &[Option<char>] {
         let start = (rank - 1) * 8;
-        // println!("{} {}", rank, start);
         &self.board[start..start + 8]
+    }
+    fn get_piece_graphic(piece: char) -> char {
+        match piece {
+            'r' => '\u{265C}',
+            'R' => '\u{2656}',
+            'n' => '\u{265E}',
+            'N' => '\u{2658}',
+            'b' => '\u{265D}',
+            'B' => '\u{2657}',
+            'k' => '\u{265A}',
+            'K' => '\u{2654}',
+            'q' => '\u{265B}',
+            'Q' => '\u{2655}',
+            'p' => '\u{265F}',
+            'P' => '\u{2659}',
+            _ => 'X',
+        }
     }
 
     pub fn draw(&self) -> String {
         let mut output: String = "".to_owned();
-        // for square in self.board {
-        //     output = output + "."
-        // }
-        // for (i, square) in self.board.iter().enumerate() {
+
+        output.push_str("  a b c d e f g h\n");
+
         for rank in (1..9).rev() {
+            output.push_str(&format!("{} ", rank));
             for (i, square) in self.get_rank(rank).iter().enumerate() {
-                if *square == 0u32 {
+                if *square == None {
                     if (i + rank) % 2 == 0 {
-                        output += "⬜"; //\u{0305}"
+                        output.push('⬜');
                     } else {
-                        output += "⬛"
+                        output.push('⬛');
                     }
                 } else {
-                    //
-                    output += "♞ "
+                    let piece = square.unwrap();
 
-                    //output += &square.to_string().to_owned();
+                    output.push(ChessBoard::get_piece_graphic(piece));
+                    output.push(' ');
                 }
             }
+            output.push_str(&format!(" {} ", rank));
+
             output += "\n";
         }
-        // for (i, square) in self.get_rank(8).iter().enumerate() {
-        //     output += &square.to_string()
-        //     // if i >> 4 == 0 {
-        //     //     output += "."
-        //     // }
+        output.push_str("  a b c d e f g h");
 
-        //     // if i % 8 == 0 {
-        //     //     output += "\n";
-        //     //     continue;
-        //     // }
-        //     // output = output + &square.to_string()
-        //     // output = output + &i.to_string()
-        // }
         return output;
     }
 }
@@ -91,13 +96,11 @@ impl Default for ChessBoard {
 }
 
 fn main() {
-    // let chess_board = ChessBoard::new();
     let chess_board =
         ChessBoard::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     let ui = chess_board.draw();
 
     println!("♞  Dan's Rusty Chess ♞\n");
-    println!("I\u{0305}V\u{0305} - I̅V̅");
     println!("{}", ui);
 }
 
